f27d77a1683f8562cb72e6421a3650c4816a4e11
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2020-12-06 17:20:28 -0500

    Update TeamCard.vue

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-
+        <seccion class="mb-5" width="100%">
+          <img v-bind:src="member.image" width="100%">
+          <h2 class="text-center">
+            Team member: {{member.codigo}} {{member.rol}}
+          </h2>
+          <h2 class="text-center">
+            {{member.nombres}}
+          </h2>
+          <p class="text-justify">
+            {{member.descripcion}}
+          </p>
+        </seccion>
     </div>
 </template>
 
